841715fd4f3d5954b13b6bdbdcd9e1abfa276871
Dhruv Govani <dhruvgovani@icloud.com>
2020-06-07 16:34:04 +0530

    Create OSNotifier.swift

---
+++ OSNotifier.swift
@@ -0,0 +1,106 @@
+//
+//  OSNotifier.swift
+//  DGOnScreenNotifier
+//
+//  Created by Dhruv Govani on 06/06/20.
+//  Copyright © 2020 Dhruv Govani. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum direction {
+    case top, bottom
+}
+
+extension UIView{
+    
+    /**
+     ParantView will be : self.view, this function will initialize the view.
+     */
+    func initOSNotifier(ParantView : UIView, Text : String, Icon : UIImage, BackgroundColor : UIColor, AppearFrom : direction, TextColor : UIColor){
+        
+        switch AppearFrom {
+        case .bottom:
+             self.frame = CGRect(x: 15, y: UIScreen.main.bounds.height, width: UIScreen.main.bounds.width - 30, height: 50)
+        case .top:
+             self.frame = CGRect(x: 15, y: 0, width: UIScreen.main.bounds.width - 30, height: 50)
+        }
+        
+        self.backgroundColor = BackgroundColor
+        self.clipsToBounds = true
+        self.layer.cornerRadius = 8
+        self.alpha = 0
+        
+        let icon = UIImageView()
+        icon.frame = CGRect(x: 12, y: 10, width: 30, height: 30)
+        icon.image = Icon
+        icon.contentMode = .scaleAspectFit
+        self.addSubview(icon)
+        
+        
+        let title = UILabel()
+        
+        title.frame = CGRect(x: 12 + icon.frame.width + 8 , y: 10, width: self.frame.width - icon.frame.width - 28, height: self.frame.height - 20)
+        
+        title.numberOfLines = 0
+        title.textColor = TextColor
+        title.lineBreakMode = .byClipping
+        title.text = Text
+        title.textAlignment = .left
+        title.adjustsFontSizeToFitWidth = true
+        title.minimumScaleFactor = 0.25
+        title.numberOfLines = 0
+        title.layoutIfNeeded()
+        
+        self.addSubview(title)
+        
+    }
+    /**
+    ParantView will be : self.view, this function will show and hide the notifier with animations
+    */
+    func showOSNotifier(to : direction,ParantView : UIView){
+        
+         var startPos : CGFloat = 0
+        
+        
+        switch to {
+        case .top:
+            startPos = ParantView.safeAreaLayoutGuide.layoutFrame.minY + 10
+        case .bottom:
+            startPos = ParantView.safeAreaLayoutGuide.layoutFrame.maxY - 70
+        }
+        
+        var defaultPos : CGFloat = 0
+        
+        switch to {
+        case .top:
+            defaultPos = 0
+        case .bottom:
+            defaultPos = UIScreen.main.bounds.height
+        }
+        
+        UIView.transition(with: ParantView, duration: 0.2, options: .transitionCrossDissolve, animations: {
+            self.alpha = 1
+             self.frame = CGRect(x: 15, y: startPos, width: UIScreen.main.bounds.width - 30, height: 50)
+           
+        }) { (_) in
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                
+                UIView.transition(with: ParantView, duration: 0.3, options: .curveEaseOut, animations: {
+                    self.alpha = 0
+                }) { (_) in
+                    
+                    self.frame = CGRect(x: 15, y: defaultPos, width: UIScreen.main.bounds.width - 30, height: 50)
+                }
+                
+            }
+            
+        }
+        
+        
+    }
+    
+
+}
